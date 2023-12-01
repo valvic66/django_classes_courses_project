1046d110468dcaab0b4e8a6d19ca3cd4491dc7ba
Valentin Micu <valvic6@gmail.com>
2023-12-01 23:38:38 +0200

    added bootstrap; added upload form on a new url

--- classes/urls.py
+++ classes/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', MyClassListView.as_view(), name='class_list'),
     path('class/<int:class_id>/all_courses/', CourseListView.as_view(), name='course_list'),
     path('class/<int:class_id>/course/<int:course_id>/all_pdfs/', PDFFileListView.as_view(), name='pdf_list'),
-    path('classes/<int:class_id>/courses/<int:course_id>/upload-pdf/', UploadPDFFileView.as_view(), name='upload_pdf_file'),
+    path('class/<int:class_id>/courses/<int:course_id>/upload-pdf/', UploadPDFFileView.as_view(), name='upload_pdf_file'),
 ]
