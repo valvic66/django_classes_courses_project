f6865060cf0ced6c5de153389d036539e7d4ee37
Valentin Micu <valvic6@gmail.com>
2023-12-01 19:56:23 +0200

    fixed the url as needed

--- classes/urls.py
+++ classes/urls.py
@@ -5,8 +5,8 @@ from .views import MyClassListView, CourseListView, PDFFileListView, UploadPDFFi
 
 urlpatterns = [
     path('', MyClassListView.as_view(), name='class_list'),
-    path('class/<int:class_id>/courses/', CourseListView.as_view(), name='course_list'),
-    path('courses/<int:course_id>/pdfs/', PDFFileListView.as_view(), name='pdf_list'),
+    path('class/<int:class_id>/all_courses/', CourseListView.as_view(), name='course_list'),
+    path('class/<int:class_id>/course/<int:course_id>/all_pdfs/', PDFFileListView.as_view(), name='pdf_list'),
     path('courses/<int:course_id>/upload-pdf/', UploadPDFFileView.as_view(), name='upload_pdf_file'),
     # ... other URL patterns
 ]
