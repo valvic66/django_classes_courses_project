fd20d933821a42e81103da60676440a2f2b9b374
Valentin Micu <valvic6@gmail.com>
2023-12-01 19:36:45 +0200

    urls changes

--- classes/urls.py
+++ classes/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from .views import MyClassListView, CourseListView, PDFFileListView, UploadPDFFileView
 
 urlpatterns = [
-    path('classes/', MyClassListView.as_view(), name='class_list'),
-    path('classes/<int:class_id>/courses/', CourseListView.as_view(), name='course_list'),
+    path('', MyClassListView.as_view(), name='class_list'),
+    path('class/<int:class_id>/courses/', CourseListView.as_view(), name='course_list'),
     path('courses/<int:course_id>/pdfs/', PDFFileListView.as_view(), name='pdf_list'),
     path('courses/<int:course_id>/upload-pdf/', UploadPDFFileView.as_view(), name='upload_pdf_file'),
     # ... other URL patterns
